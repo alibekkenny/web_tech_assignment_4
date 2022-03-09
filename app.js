8569fc16c1dbb0c7c34c36687a3aa84f210abb72
alibekkenny <topskiali@gmail.com>
2022-03-09 17:47:50 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var users = [{
+        nickname: "Erwin",
+        email: "smith@gmail.com",
+        password: "12345",
+    },
+    {
+        nickname: "Garou",
+        email: "garou@gmail.com",
+        password: "12345",
+    },
+
+];
+var signedUser = users[0];
+
+var reviews = [{
+        nickname: "Erwin_Smith",
+        feedback: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nostrum numquam, error deleniti amet culpa corrupti iusto sit molestiae, impedit omnis sed hic magni iste consequuntur! Tempora officiis aliquam maxime! Ipsum consequatur quidem in labore pariatur nisi molestiae illo repellendus, esse incidunt temporibus vitae perferendis earum modi facilis doloremque aspernatur eos perspiciatis quas. Ea rem perferendis atque a amet eius.",
+        image_path: "src/erwin_smith.jpg",
+        rating: 4,
+    },
+    {
+        nickname: "Garou_TheHeroHunter",
+        feedback: "Very good site! I would appoint this guy the director of Google! What is only great anime references as Code Geass worth!",
+        image_path: "src/garou.png",
+        rating: 5
+    },
+    {
+        nickname: "Deidara",
+        feedback: "Beautiful... This is... art!",
+        image_path: "src/deidara.jpg",
+        rating: 5,
+    }
+
+]
